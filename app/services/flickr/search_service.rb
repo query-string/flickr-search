fc147e272859934be6658e196483d66c14d566dc
Alex Timofeev <atimofeev@reactant.ru>
2018-04-17 15:15:25 +0700

    Don't let an empty query to be sent

--- app/services/flickr/search_service.rb
+++ app/services/flickr/search_service.rb
@@ -2,7 +2,9 @@
 
 module Flickr
   module SearchService
-    def self.call(query, scope = :photos, level = 3)
+    def self.call(query, preload = 3)
+      raise ArgumentError, 'Search query can`t be empty' if query.empty?
+      Flickr.photos.search(text: query, sort: :relevance, per_page: 50)
     end
   end
 end
--- spec/services/flickr/search_service_spec.rb
+++ spec/services/flickr/search_service_spec.rb
@@ -3,12 +3,25 @@
 require 'rails_helper'
 
 describe Flickr::SearchService do
+  subject { Flickr::SearchService.(query) }
+
   context 'with valid API key' do
     context 'default query' do
-      subject { Flickr::SearchService.('Bangkok') }
+      let(:query) { 'Bangkok' }
 
       it 'returns search results' do
-        expect(subject).to eq({})
+        expect(subject.length).to eq(50)
+      end
+      it 'results have expected attributes' do
+        response = subject.first
+        %i[id title url].each { |key| expect(response.send(key).present?).to be_truthy }
+      end
+    end
+
+    context 'empty query' do
+      let(:query) { '' }
+      it 'raises an exception' do
+        expect { subject }.to raise_error(ArgumentError, 'Search query can`t be empty')
       end
     end
   end
