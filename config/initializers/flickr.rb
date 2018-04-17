be4be58b23dd3952292429f7411d705c835197c1
Alex Timofeev <atimofeev@reactant.ru>
2018-04-17 14:24:30 +0700

    Setup `flickr-objects` gem

---
+++ config/initializers/flickr.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Flickr.configure do |config|
+  config.api_key       = Rails.application.credentials.flickr[:key]
+  config.shared_secret = Rails.application.credentials.flickr[:secret]
+end
