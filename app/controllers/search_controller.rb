e2e94195bea8878ff412edc9c8c6195bbb036c50
Alex Timofeev <atimofeev@reactant.ru>
2018-04-19 12:49:30 +0700

    Display search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class SearchController < ApplicationController
+  def index
+    render :index, locals: {
+      results: params[:query].present? ? Flickr::SearchService.(params[:query]) : []
+    }
+  end
 end
