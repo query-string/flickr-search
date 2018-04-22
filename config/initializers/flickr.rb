be723c6696637f2c02e966e1929fcabbd2d34a50
Alex Timofeev <atimofeev@reactant.ru>
2018-04-22 21:24:12 +0700

    Fetch Flickr keys

--- config/initializers/flickr.rb
+++ config/initializers/flickr.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Flickr.configure do |config|
-  config.api_key       = Rails.application.credentials.flickr[:key]
-  config.shared_secret = Rails.application.credentials.flickr[:secret]
+  config.api_key       = Rails.application.credentials.flickr.fetch(:key)
+  config.shared_secret = Rails.application.credentials.flickr.fetch(:secret)
   config.pagination    = :will_paginate
 end
