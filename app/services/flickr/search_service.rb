afedbb62c7013270f8d0fc49304fb4d5d2028ca9
Alex Timofeev <atimofeev@reactant.ru>
2018-04-22 08:29:27 +0700

    Tidy up SearchService a bit

--- app/services/flickr/search_service.rb
+++ app/services/flickr/search_service.rb
@@ -1,13 +1,31 @@
 # frozen_string_literal: true
 
 module Flickr
-  module SearchService
-    def self.call(attrs)
-      query = attrs[:query]
-      page  = attrs[:page]
+  class SearchService < Service
+    PER_PAGE = 50
 
-      raise ArgumentError, 'Search query can`t be empty' if query.empty?
-      Flickr.photos.search(text: query, sort: :relevance, per_page: 50, page: page)
+    attr_reader :query, :page, :response
+
+    def call(attrs)
+      @query    = attrs[:query]
+      @page     = attrs[:page]
+      @response = { results: [], errors: [] }
+
+      return response if query.empty?
+      request
+    end
+
+    private
+
+    def request
+      begin
+        response[:results] = Flickr.photos.search(text: query, sort: :relevance, per_page: PER_PAGE, page: page)
+      rescue e
+        # It would be great to call some kind of developers notification service (AppSignal, Honeybadger, etc.)
+        response[:errors] << 'Flickr API is not available at the moment. Please try again later.'
+      end
+
+      response
     end
   end
 end
